c20546d15d29d96bb58b24e04d1204358f7b9e5b
hitos <hitos@LAPTOP-STVRJLHH>
2023-10-16 22:39:30 +0900

    プロジェクト ファイルを追加します。

---
+++ CsvReadWrite/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CsvReadWrite
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBoxInputCSVFileName = new TextBox();
+            buttonCsvRead = new Button();
+            buttonCsvWrite = new Button();
+            textBoxOutputCSVFileName = new TextBox();
+            dataGridViewCsv = new DataGridView();
+            openFileDialogCsv = new OpenFileDialog();
+            saveFileDialogCsv = new SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCsv).BeginInit();
+            SuspendLayout();
+            // 
+            // textBoxInputCSVFileName
+            // 
+            textBoxInputCSVFileName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxInputCSVFileName.Location = new Point(12, 12);
+            textBoxInputCSVFileName.Name = "textBoxInputCSVFileName";
+            textBoxInputCSVFileName.Size = new Size(628, 23);
+            textBoxInputCSVFileName.TabIndex = 0;
+            // 
+            // buttonCsvRead
+            // 
+            buttonCsvRead.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonCsvRead.Location = new Point(646, 12);
+            buttonCsvRead.Name = "buttonCsvRead";
+            buttonCsvRead.Size = new Size(142, 23);
+            buttonCsvRead.TabIndex = 1;
+            buttonCsvRead.Text = "CSV取得";
+            buttonCsvRead.UseVisualStyleBackColor = true;
+            buttonCsvRead.Click += buttonCsvRead_Click;
+            // 
+            // buttonCsvWrite
+            // 
+            buttonCsvWrite.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCsvWrite.Location = new Point(646, 394);
+            buttonCsvWrite.Name = "buttonCsvWrite";
+            buttonCsvWrite.Size = new Size(142, 23);
+            buttonCsvWrite.TabIndex = 3;
+            buttonCsvWrite.Text = "CSV出力";
+            buttonCsvWrite.UseVisualStyleBackColor = true;
+            buttonCsvWrite.Click += buttonCsvWrite_Click;
+            // 
+            // textBoxOutputCSVFileName
+            // 
+            textBoxOutputCSVFileName.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxOutputCSVFileName.Location = new Point(12, 394);
+            textBoxOutputCSVFileName.Name = "textBoxOutputCSVFileName";
+            textBoxOutputCSVFileName.Size = new Size(628, 23);
+            textBoxOutputCSVFileName.TabIndex = 2;
+            // 
+            // dataGridViewCsv
+            // 
+            dataGridViewCsv.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridViewCsv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCsv.Location = new Point(11, 49);
+            dataGridViewCsv.Name = "dataGridViewCsv";
+            dataGridViewCsv.RowTemplate.Height = 25;
+            dataGridViewCsv.Size = new Size(777, 325);
+            dataGridViewCsv.TabIndex = 4;
+            // 
+            // openFileDialogCsv
+            // 
+            openFileDialogCsv.FileName = "*.csv";
+            openFileDialogCsv.InitialDirectory = ".\\";
+            // 
+            // saveFileDialogCsv
+            // 
+            saveFileDialogCsv.Filter = "CSVファイル|*.csv|すべてのファイル|*.*";
+            saveFileDialogCsv.InitialDirectory = ".\\";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridViewCsv);
+            Controls.Add(buttonCsvWrite);
+            Controls.Add(textBoxOutputCSVFileName);
+            Controls.Add(buttonCsvRead);
+            Controls.Add(textBoxInputCSVFileName);
+            Name = "Form1";
+            Text = "CSVの読み書き";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCsv).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBoxInputCSVFileName;
+        private Button buttonCsvRead;
+        private Button buttonCsvWrite;
+        private TextBox textBoxOutputCSVFileName;
+        private DataGridView dataGridViewCsv;
+        private OpenFileDialog openFileDialogCsv;
+        private SaveFileDialog saveFileDialogCsv;
+    }
+}
---
+++ CsvReadWrite/Form1.cs
@@ -0,0 +1,87 @@
+using Csv;          // ライブラリcsvを使用するのに必要
+using System.Data;  // DataTableを使うのに必要
+using System.Text;  // Encodingを使うのに必要
+namespace CsvReadWrite
+{
+    public partial class Form1 : Form
+    {
+        // 内部でデータを保持するテーブルを用意します。
+        DataTable dataTable = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // CSV取得ボタンクリック時の処理
+        private void buttonCsvRead_Click(object sender, EventArgs e)
+        {
+            // ファイルを開くウィンドウでCSVファイルを選択し。OKボタンをクリックした時
+            if(openFileDialogCsv.ShowDialog() == DialogResult.OK)
+            {
+                // ファイルを開くウィンドウで選んだCSVのファイル名をテキストボックスに反映
+                textBoxInputCSVFileName.Text = openFileDialogCsv.FileName;
+                // ファイルの全内容を文字列に読み込みます。日本語が読めこめるように、文字はutf-16でエンコードします。
+                string csv = File.ReadAllText(openFileDialogCsv.FileName,Encoding.GetEncoding("utf-16"));
+
+                dataTable.TableName = "CSVTable";   // 内部でテーブル生成します。
+                dataTable.Columns.Clear();          // 内部テーブルへのヘッダーを初期化(2連続で読み込んだ時の反応)
+                dataTable.Clear();                  // 内部テーブルのデータを初期化(2連続で読み込んだ時の反応)
+
+                // CSVからヘッダー部分のデータを取得し、内部のテーブルのカラムに設定
+                // csvから1行取得し、結果をline変数に入れる。
+                foreach(ICsvLine line in CsvReader.ReadFromText(csv))
+                {
+                    // 1行分のデータのヘッダーの情報を取り出す
+                    foreach (var item in line.Headers)
+                    {
+                        dataTable.Columns.Add(item);    // 内部のテーブルのカラムに設定
+                    }
+                    break;
+                }
+                // 読み込んだcsvのデータを内部のテーブルに割り当てる
+                foreach (ICsvLine line in CsvReader.ReadFromText(csv))
+                {
+                    dataTable.Rows.Add(line.Values);    // 1レコード分まとめて設定
+                }
+                dataGridViewCsv.DataSource = dataTable; // 表示用のDataGridViewに内部のテーブルを割当
+            }
+        }
+        
+        // CSV取込ボタンクリック時の処理
+        private void buttonCsvWrite_Click(object sender, EventArgs e)
+        {
+            // ファイルを保存するウィンドウでCSVファイルを選択し、OKボタンをクリックしたとき
+            if(saveFileDialogCsv.ShowDialog() == DialogResult.OK)
+            {
+                // ファイルを保存するウィンドウで選んだCSVのファイル名をテキストボックスに反映
+                textBoxOutputCSVFileName.Text = saveFileDialogCsv.FileName;
+                // headerという変数に内部のテーブルのカラム名を設定
+                string[] header = new string[dataTable.Columns.Count];
+                // カラムの数だけループしてカラムのデータを設定
+                for (int i = 0; i < dataTable.Columns.Count; i++)
+                {
+                    header[i] = dataTable.Columns[i].ColumnName;
+                }
+                // newLineという変数に内部のテーブルを表のイメージ(2次元配列)で設定
+                string[][] newLine = new string[dataTable.Rows.Count][];
+                // データの数分だけループして、データを取得
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    newLine[i] = new string[dataTable.Columns.Count];
+                    // 該当するカラム、列の値を内部のテーブルから、newLineに設定
+                    for (int j = 0; j < newLine[i].Length; j++)
+                    {
+                        // nullの場合は、""としてnewLineに設定
+                        newLine[i][j] = (string)dataTable.Rows[i][j] ?? "";
+                    }
+                }
+
+                // データからCSV形式の文字列を生成します
+                string outcsv = CsvWriter.WriteToText(header, newLine);
+                // FileNameという名前で、outcsvの値を保存します。文字はutf-16でエンコードします。
+                File.WriteAllText(saveFileDialogCsv.FileName, outcsv, Encoding.GetEncoding("utf-16"));
+            }
+        }
+    }
+}
