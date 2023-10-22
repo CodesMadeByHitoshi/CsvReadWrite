1b5a29d32d0eb2139e06e7b5549bc4d988fffcd5
Hitoshi Yoshitsugu <hycbuisnessmode@gmail.com>
2023-10-23 01:04:11 +0900

    Fix comments

--- CsvReadWrite/Form1.cs
+++ CsvReadWrite/Form1.cs
@@ -15,46 +15,57 @@ namespace CsvReadWrite
             InitializeComponent();
         }
 
-        // CSV取得ボタンクリック時の処理
+        /// <summary>
+        /// 選択したCSVを取込、データテーブルとして出力します。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonCsvRead_Click(object sender, EventArgs e)
         {
-            // ファイルを開くウィンドウでCSVファイルを選択し。OKボタンをクリックした時
+            // ファイルを開くウィンドウでCSVファイルを選択し、OKボタンをクリックした時。
             if (openFileDialogCsv.ShowDialog() == DialogResult.OK)
             {
-                // ファイルを開くウィンドウで選んだCSVのファイル名をテキストボックスに反映
+                // ファイルを開くウィンドウで選んだCSVのファイル名をテキストボックスに反映。
                 textBoxInputCSVFileName.Text = openFileDialogCsv.FileName;
                 // ファイルの全内容を文字列に読み込みます。日本語が読めこめるように、文字はutf-16でエンコードします。
                 string csv = File.ReadAllText(openFileDialogCsv.FileName, Encoding.GetEncoding("utf-16"));
 
                 ClearDataTable();
 
-                // CSVからヘッダー部分のデータを取得し、内部のテーブルのカラムに設定
+                // CSVからヘッダー部分のデータを取得し、内部のテーブルのカラムに設定。
                 // csvから1行取得し、結果をline変数に入れる。
                 foreach (ICsvLine line in CsvReader.ReadFromText(csv))
                 {
-                    // 1行分のデータのヘッダーの情報を取り出す
+                    // 1行分のデータのヘッダーの情報を取り出します。
                     foreach (var item in line.Headers)
                     {
-                        dataTable.Columns.Add(item);    // 内部のテーブルのカラムに設定
+                        // 内部のテーブルのカラムに設定。
+                        dataTable.Columns.Add(item);    
                     }
                     break;
                 }
                 // 読み込んだcsvのデータを内部のテーブルに割り当てる
                 foreach (ICsvLine line in CsvReader.ReadFromText(csv))
                 {
-                    dataTable.Rows.Add(line.Values);    // 1レコード分まとめて設定
+                    // 1レコード分まとめて設定。
+                    dataTable.Rows.Add(line.Values);    
                 }
-                dataGridViewCsv.DataSource = dataTable; // 表示用のDataGridViewに内部のテーブルを割当
+                // 表示用のDataGridViewに内部のテーブルを割当。
+                dataGridViewCsv.DataSource = dataTable; 
             }
         }
 
-        // CSV取込ボタンクリック時の処理
+        /// <summary>
+        /// データテーブル状のデータCSVファイルとして出力します。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>        
         private void buttonCsvWrite_Click(object sender, EventArgs e)
         {
-            // ファイルを保存するウィンドウでCSVファイルを選択し、OKボタンをクリックしたとき
+            // ファイルを保存するウィンドウでCSVファイルを選択し、OKボタンをクリックしたとき。
             if (saveFileDialogCsv.ShowDialog() == DialogResult.OK)
             {
-                // ファイルを保存するウィンドウで選んだCSVのファイル名をテキストボックスに反映
+                // ファイルを保存するウィンドウで選んだCSVのファイル名をテキストボックスに反映。
                 textBoxOutputCSVFileName.Text = saveFileDialogCsv.FileName;
                 // DataGridViewの内容を取得
                 dataTable = (DataTable)dataGridViewCsv.DataSource;
@@ -66,16 +77,16 @@ namespace CsvReadWrite
                 {
                     header[i] = dataTable.Columns[i].ColumnName;
                 }
-                // newLineという変数に内部のテーブルを表のイメージ(2次元配列)で設定
+                // newLineという変数に内部のテーブルを表のイメージ(2次元配列)で設定。
                 string[][] newLine = new string[dataTable.Rows.Count][];
                 // データの数分だけループして、データを取得
                 for (int i = 0; i < dataTable.Rows.Count; i++)
                 {
                     newLine[i] = new string[dataTable.Columns.Count];
-                    // 該当するカラム、列の値を内部のテーブルから、newLineに設定
+                    // 該当するカラム、列の値を内部のテーブルから、newLineに設定。
                     for (int j = 0; j < newLine[i].Length; j++)
                     {
-                        // nullの場合は、""としてnewLineに設定
+                        // nullの場合は、""としてnewLineに設定。
                         if (dataTable.Rows[i][j] == DBNull.Value)
                         {
                             newLine[i][j] = "";
@@ -87,39 +98,42 @@ namespace CsvReadWrite
                     }
                 }
 
-                // データからCSV形式の文字列を生成します
+                // データからCSV形式の文字列を生成します。
                 string outcsv = CsvWriter.WriteToText(header, newLine);
                 // FileNameという名前で、outcsvの値を保存します。文字はutf-16でエンコードします。
                 File.WriteAllText(saveFileDialogCsv.FileName, outcsv, Encoding.GetEncoding("utf-16"));
             }
         }
-
+        /// <summary>
+        /// CSVファイルを新規作成するためのデータテーブルを作成します。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonCsvCreate_Click(object sender, EventArgs e)
         {
- 
-
             try
             {
                 ClearDataTable();
 
-                // 数値チェックをします
+                // 数値チェックをします。
                 int newColumnLength = Convert.ToInt32(textBoxColumnLength.Text);
                 int newRowLength = Convert.ToInt32(textBoxRowLength.Text);
                 DataTable dtForNewCsv = new DataTable();
 
-                // 行の名称設定
+                // 行の名称設定。
                 for(int i = 1;i <= newColumnLength;i++)
                 {
                     dtForNewCsv.Columns.Add("データ" + i.ToString(), typeof(String));
                 }
 
-                // 列の値を設定
+                // 列の値を設定。
                 for (int i = 0; i < newRowLength; i++)
                 {
                     dtForNewCsv.Rows.Add("");
                 }
 
-                dataGridViewCsv.DataSource = dtForNewCsv; // 表示用のDataGridViewに内部のテーブルを割当
+                // 表示用のDataGridViewに内部のテーブルを割当。
+                dataGridViewCsv.DataSource = dtForNewCsv;
 
             }
             catch (FormatException)
